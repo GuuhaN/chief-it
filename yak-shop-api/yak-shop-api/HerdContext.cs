6c8daef8ccfbf6a86ba0796518d6fd27baa192c6
Nigel Cheung <nigelcheung97@hotmail.com>
2021-09-30 22:54:52 +0200

    RestApi - First build backend

---
+++ yak-shop-api/yak-shop-api/HerdContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using yak_shop_api.Models;
+
+namespace yak_shop_api
+{
+    public class HerdContext : DbContext
+    {
+        public HerdContext(DbContextOptions<HerdContext> options ): base(options) { }
+
+        public DbSet<Herd> Herds { get; set; }
+        public DbSet<Yak> Yaks { get; set; }
+        public DbSet<CustomerOrder> Orders { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
